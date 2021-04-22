8eb7323c16d686ca3aaee85f0366a698ca6f9392
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2021-04-22 15:33:28 +0200

    moved from dropdown to shelf
    now the dropdown will now where to move afterwards

--- src/Shelf.js
+++ src/Shelf.js
@@ -18,9 +18,9 @@ class Shelf extends React.Component {
       
             <div>
             <Book allBooks= {allBooks} />
-            <Book books = {currentlyReading} title ={"Currently Reading"}/>
-            <Book books = {wantToRead} title = {"Want to read"}/>
-            <Book books = {read} title = {"Read"}/>
+            <Book books = {currentlyReading} title ={"Currently Reading"} changeShelf={this.props.changeShelf}/>
+            <Book books = {wantToRead} title = {"Want to read"} changeShelf={this.props.changeShelf}/>
+            <Book books = {read} title = {"Read"} changeShelf={this.props.changeShelf}/>
             </div>
        
     );
