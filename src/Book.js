8aec24632d8aed3d92563432c365782da829ce1b
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2021-05-11 14:14:15 +0200

    added conditional statements to check if no image found, then display n/a

--- src/Book.js
+++ src/Book.js
@@ -4,37 +4,41 @@ import Search from "./Search.js";
 import * as BooksAPI from "./BooksAPI";
 import { Link } from "react-router-dom";
 import Header from "./Header";
+import noCover from "./images/notfound.png";
+
+
 
 class Book extends React.Component {
+  
   render() {
-    console.log(this.props)
-    var books = this.props.books
+    console.log(this.props);
+    var books = this.props.books;
+    const coverImg =
+    books.imageLinks && books.imageLinks.thumbnail
+      ? books.imageLinks.thumbnail
+      : noCover;
+  const title = books.title ? books.title : 'No title available';
+  const author = books.author ? books.author : "N/A"
+    
     return (
       <div className="list-books">
- 
-          
-        
-              <div className="book">
-                <div className="book-top">
-                  <div
-                    className="book-cover"
-                    style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: `url(${
-                        books.imageLinks.thumbnail
-                      })`,
-                    }}
-                  />
-                  <Dropdown
-                    books={books}
-                    changeShelf={this.props.changeShelf}
-                  />
-                </div>
-                <div className="book-title">{books.title}</div>
-                <div className="book-authors">{books.authors}</div>
-              </div>
-        
+        <div className="book">
+      
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${coverImg})`,
+              }}
+            />
+            <Dropdown books={books} changeShelf={this.props.changeShelf} />
+          </div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{books.author}</div>
+        </div>
+
         <div className="open-search">
           <Link to="/search">Add a book</Link>
         </div>
